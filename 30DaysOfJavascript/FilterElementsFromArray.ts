092bedee84f84aea7983b30eaee976e762034879
kadeem-lewis <email@kadeemlewis.com>
2024-01-20 23:49:18 -0500

    completed filter array leetcode

--- 30DaysOfJavascript/FilterElementsFromArray.ts
+++ 30DaysOfJavascript/FilterElementsFromArray.ts
@@ -1,3 +1,11 @@
 type Fn = (n: number, i: number) => any;
 
-function filter(arr: number[], fn: Fn): number[] {}
+function filter(arr: number[], fn: Fn): number[] {
+  const returnedArray: number[] = [];
+  for (let i = 0; i < arr.length; i++) {
+    if (fn(arr[i], i)) {
+      returnedArray.push(arr[i]);
+    }
+  }
+  return returnedArray;
+}
